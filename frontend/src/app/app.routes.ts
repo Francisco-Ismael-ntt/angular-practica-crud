38fc582649b1c4c53e13f3be749754e9a52d6482
fismael <fmartalo@emeal.nttdata.com>
2025-05-15 14:35:48 +0200

    conflicts solved

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
-<<<<<<< Updated upstream
-
-export const routes: Routes = [];
-=======
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { CarDetailComponent } from './components/car-detail/car-detail.component';
 
@@ -11,4 +7,4 @@ export const routes: Routes = [
   {path:'',component: HomePageComponent},
   {path:'cars/:id',component: CarDetailComponent}
 ];
->>>>>>> Stashed changes
+
