ad45c467eb77c8fb4f55e8cc269b04a9aa4e8b63
fismael <fmartalo@emeal.nttdata.com>
2025-05-15 13:51:12 +0200

    fix merge conflicts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CustomButtonDirective } from './directives/custom-button.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CustomButtonDirective],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
