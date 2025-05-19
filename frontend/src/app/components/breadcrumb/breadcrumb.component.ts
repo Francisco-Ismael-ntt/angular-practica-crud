cac4eb1e1d339f77fb10b1aebfed182572bdc207
fismael <fmartalo@emeal.nttdata.com>
2025-05-19 15:45:48 +0200

    creacion y implementacion del componente breadcrumb para la navegacion

---
+++ frontend/src/app/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,36 @@
+import { Component, Pipe } from '@angular/core';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-breadcrumb',
+  imports: [RouterLink],
+  templateUrl: './breadcrumb.component.html',
+  styleUrl: './breadcrumb.component.css'
+})
+export class BreadcrumbComponent {
+  breadcrumb: {label: string, url: string}[] = []
+
+  constructor(router: Router){
+    router.events.pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(()=>{
+
+      const currentUrl = router.url.split('/').filter(Boolean)
+      this.breadcrumb = []
+      const baseBreadcrumb = [
+        {label: 'Home', url: '/'},
+        {label: 'Detalles', url: '/cars'},
+        {label: 'Editar coche', url: '/edit'}
+      ]
+
+      if(currentUrl.includes('cars')){
+        this.breadcrumb.push(baseBreadcrumb[0], baseBreadcrumb[1])
+      } else {
+        this.breadcrumb.push(baseBreadcrumb[0])
+      }
+
+    })
+  }
+
+
+}
--- frontend/src/app/components/table-component/table-component.component.ts
+++ frontend/src/app/components/table-component/table-component.component.ts
@@ -8,20 +8,6 @@ import { TagModule } from 'primeng/tag';
 import { CarModel } from '../../models/car-model';
 import { MileagePipe } from '../../pipes/mileage.pipe';
 
-// import { CdkMenu, CdkMenuItem, CdkMenuGroup, CdkMenuTrigger } from '@angular/cdk/menu';
-
-/**
- * Logica de la tabla
- * pasar 2 arrays
- *  array 1 cabecera
- *  array 2 objetos para dibujar
- *
- * en funion de array 1 por la cuenta de objetos se sabra cada cuanto tien que hacer el salto array 2 para dibujar los objetos en su sitio
- *
- * si un objeto es un string que empieza por / es un enlace
- * si un objetos es una funcion es un boton
- */
-
 
 @Component({
   selector: 'app-table-component',
