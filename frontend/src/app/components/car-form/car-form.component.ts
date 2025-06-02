5195e7a1f93eb13076b9b9bd05256d4f9026abc6
fismael <fmartalo@emeal.nttdata.com>
2025-06-02 13:28:30 +0200

    * se ha implementado la logica necesaria para poder añadir varios formGroups dentro del formArray * se ha ordenado el codigo separandolo en mas funciones
    TODO
    * rellenar el formulario con los datos del coche que viene desde backend cuando el usuario lo este editando

--- frontend/src/app/components/car-form/car-form.component.ts
+++ frontend/src/app/components/car-form/car-form.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, inject, Input} from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl, FormArray } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl, FormArray, FormRecord } from '@angular/forms';
 import { CarService } from '../../services/car.service';
 import { CreateCarDto } from '../../models/create-car-model';
 import { CarDetailsDto } from '../../models/car-details-model';
@@ -29,7 +29,7 @@ export class CarFormComponent {
   car!: CarModel
 
   fb = new FormBuilder
-  carForm: FormGroup = this.fb.group({
+  carForm: FormRecord = this.fb.group({
     brand: this.fb.control('',[Validators.required]),
     model: this.fb.control('',[Validators.required]),
     details: this.fb.array([
@@ -46,8 +46,9 @@ export class CarFormComponent {
       )
     ])
   })
-  iterateCarDetails: any[] = [this.carForm.controls['details']]
-
+  details():FormArray{
+    return <FormArray>this.carForm.get('details')
+  }
 
 
   ngOnInit(){
@@ -55,61 +56,17 @@ export class CarFormComponent {
       if(params['id']){
         console.log(params['id'])
         this.carId = params['id']
+        this.getBrands()
         this.getCarById(this.carId)
       } else {
         console.log('no car id')
         this.getBrands()
-        // this.carForm = this.createCarForm()
       }
     })
 
 
   }
-/**
-  editForm(car: CarModel): FormGroup{
-    const form = this.fb.group({
-      brand: this.fb.control(car.brand, [Validators.required]),
-      model: this.fb.control(car.model, [Validators.required]),
 
-      details: this.fb.array([])
-    })
-
-    car.carDetails.forEach((element)=>{
-      const detail = this.fb.group({
-        registerDate: this.fb.control(element.registrationDate || '', [Validators.required]),
-        manufactureYear: this.fb.control(element.manufactureYear || 0, [Validators.required]),
-        currency: this.fb.control(element.currency || '', [Validators.required]),
-        price: this.fb.control(element.price || 0, [Validators.required]),
-        licensePlate: this.fb.control(element.licensePlate || '', [Validators.required]),
-        milleage: this.fb.control(element.mileage || 0, [Validators.required]),
-        avaiability: this.fb.control(element.availability || false, [Validators.required]),
-      })
-      // this.carForm.details.push(detail)
-    })
-
-    return form
-  }
-  */
-
-  // createCarForm(): FormGroup {
-
-  //   const detailsGroup = this.fb.group({
-  //     registerDate:  this.fb.control('',[Validators.required]),
-  //     manufactureYear: this.fb.control(0,[Validators.required]),
-  //     currency: this.fb.control('',[Validators.required]),
-  //     price: this.fb.control(0,[Validators.required]),
-  //     licensePlate: this.fb.control('',[Validators.required]),
-  //     milleage: this.fb.control(0,[Validators.required]),
-  //     avaiability: this.fb.control(false, [Validators.required])
-  //   })
-
-  //   let form = this.fb.group({
-  //     brand: this.fb.control('',[Validators.required]),
-  //     model: this.fb.control('',[Validators.required]),
-  //     details: this.fb.array([detailsGroup])
-  //   })
-  //   return form
-  // }
 
 
   /** CREACION Y MODIFICACION DEL FORMULARIO */
@@ -124,58 +81,33 @@ export class CarFormComponent {
         avaiability: this.fb.control(false, [Validators.required])
     });
 
-    (<FormArray>this.carForm.controls['details']).push(newCarDetail)
+    this.details().push(newCarDetail)
 
-    this.updateIterateCarDetails()
+
   }
-  updateIterateCarDetails(){
-    this.iterateCarDetails = []
-    this.iterateCarDetails = [this.carForm.controls['details']]
+
+  deleteDetals(index: number){
+    this.details().removeAt(index)
   }
+
 
   /** ENTRADA DE DATOS */
   collectData(): CreateCarDto {
 
-    let detailsDto: CarDetailsDto[] = [{
-      registrationDate: '',
-      mileage: 0,
-      currency: '',
-      price: 0,
-      manufactureYear: 0,
-      availability: false,
-      licensePlate: ''
-    }]
-    let carDto: CreateCarDto = {
-      brand:'',
-      model:'',
-      carDetails: detailsDto
-    }
+    let carDetailsDto: CarDetailsDto[] = []
+    let carDto!: CreateCarDto
 
-    const myBrand = this.carForm.controls['brand'].value
-    const myModel = this.carForm.controls['model'].value
+    const myBrand = this.carForm.get('brand')?.value
+    const myModel = this.carForm.get('model')?.value
 
-    const registerDateStr = this.carForm.controls['registerDate'].value
-
-    const myManufactureYear = this.carForm.controls['manufactureYear'].value
-    const myCurrency = this.carForm.controls['currency'].value
-    const myPrice = this.carForm.controls['price'].value
-    const myLicensePlate = this.carForm.controls['licensePlate'].value
-    const myMilleage = this.carForm.controls['milleage'].value
-    const myAvaiability = this.carForm.controls['avaiability'].value
-
-    console.log(myBrand)
-    console.log(myModel)
-
-    console.log(registerDateStr)
-    console.log(myManufactureYear)
-    console.log(myCurrency)
-    console.log(myPrice)
-    console.log(myLicensePlate)
-    console.log(myMilleage)
-    console.log(myAvaiability)
-
-
-    if(myBrand && myModel && registerDateStr && myManufactureYear && myCurrency && myPrice && myLicensePlate && myMilleage && myAvaiability){
+    for(let control of this.details().controls){
+      const registerDateStr = control.get('registerDate')?.value
+      const myManufactureYear = control.get('manufactureYear')?.value
+      const myCurrency = control.get('currency')?.value
+      const myPrice = control.get('price')?.value
+      const myLicensePlate = control.get('licensePlate')?.value
+      const myMilleage = control.get('milleage')?.value
+      const myAvaiability = control.get('avaiability')?.value
       const registrationDate = new Date(registerDateStr).toISOString()
       let carDetails: CarDetailsDto = {
         registrationDate: registrationDate,
@@ -187,15 +119,18 @@ export class CarFormComponent {
         licensePlate: myLicensePlate
       }
 
-      detailsDto.push(carDetails)
-
+      carDetailsDto.push(carDetails)
+      carDto = {
+        brand: myBrand,
+        model: myModel,
+        carDetails: carDetailsDto
+      }
 
     }
-
-
     return carDto
   }
   onSubmit(){
+    console.log(this.carForm)
     const newCar = this.collectData()
     if(newCar){
       console.log(newCar)
@@ -210,6 +145,23 @@ export class CarFormComponent {
     } else {
       console.log('fallo en validacion')
     }
+  }
+
+  populatingByServerData(data: CarModel){
+
+    this.carForm.removeControl('brand')
+    this.carForm.removeControl('model')
+    this.carForm.removeControl('details')
+
+    const brandControl = this.fb.control(data.brand, Validators.required)
+    const modelControl =  this.fb.control(data.model, Validators.required)
+    const detailControl = data.carDetails.map( detail => {
+      return this.fb.control(detail, Validators.required)
+    })
+
+    this.carForm.addControl('brand', brandControl)
+    this.carForm.addControl('model', modelControl)
+    this.carForm.addControl('details', this.fb.array(detailControl))
   }
 
   /** LLAMADAS API */
@@ -259,9 +211,8 @@ export class CarFormComponent {
     this.carService.getCarById(carId).subscribe({
       next:(value) => {
         this.car = value
-        this.getBrands()
         this.getModels(value.brand)
-        // this.editForm(value)
+        this.populatingByServerData(value)
       },error(err) {
         console.log(err)
       },
